1f6d705e18b59d6c4d7e73e0a0b4ffead7b00a6a
Frank Ding <dingding303@gmail.com>
2020-05-29 16:52:32 +0800

    tsp min max async version

--- tsp/ptr_net_pytorch/TSP_minmax.py
+++ tsp/ptr_net_pytorch/TSP_minmax.py
@@ -5,7 +5,6 @@ from typing import List, Tuple
 
 FLOAT_INF = float("inf")
 
-lst_coor = []
 
 def dist_tour(coordinates: List[Tuple[float, float]], tour: List[int]) -> float:
     output_dist: float = 0.0
@@ -35,6 +34,7 @@ def solve_parallel():
     start = datetime.datetime.now()
     print(start)
 
+    lst_coor = []
     with open('../tsp_10_test_sample.txt') as fp:
         for line in fp.readlines():
             input, output = line.split('output')
@@ -46,9 +46,12 @@ def solve_parallel():
             for i in range(len(points) // 2):
                 coordinates.append((points[2 * i], points[2 * i + 1]))
             lst_coor.append(coordinates)
-    print(len(lst_coor))
-    p = Pool(6)
-    p.map(solve_min_max, lst_coor)
+    # print(len(lst_coor))
+    pool = Pool(6)
+    results = [pool.apply_async(solve_min_max, args=[coor]) for coor in lst_coor]
+    # p.map(solve_min_max, lst_coor)
+    output = [p.get() for p in results]
+    print(output)
 
     end = datetime.datetime.now()
     print(end - start)

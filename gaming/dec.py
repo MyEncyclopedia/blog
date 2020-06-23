4f7ab2697ea8dc7bdfebab6fa8a2860e53a35916
Frank <dingding303@gmail.com>
2020-06-23 22:36:37 +0800

    dp vs brute trace

--- gaming/dec.py
+++ gaming/dec.py
@@ -4,92 +4,64 @@ from functools import lru_cache
 from typing import List
 
 
-from itertools import chain
-import traceback
-import sys
 
-import sys, traceback
+class _MEquals(object):
+    def __init__(self, parent_l, parent_r, pid, id):
+        self.parent_l = parent_l
+        self.parent_r = parent_r
+        self.pid = pid
+        self.id = id
 
-def print_exc_plus(  ):
-    """
-    Print the usual traceback information, followed by a listing of all the
-    local variables in each frame.
-    """
-    tb = sys.exc_info()[2]
-    while 1:
-        if not tb.tb_next:
-            break
-        tb = tb.tb_next
-    stack = []
-    f = tb.tb_frame
-    while f:
-        stack.append(f)
-        f = f.f_back
-    stack.reverse(  )
-    traceback.print_exc(  )
-    print("Locals by frame, innermost last")
-    for frame in stack:
-        print()
-        print("Frame %s in %s at line %s" % (frame.f_code.co_name, frame.f_code.co_filename, frame.f_lineno))
-        for key, value in frame.f_locals.items(  ):
-            print("\t%20s = " % key)
-            # We have to be VERY careful not to cause a new error in our error
-            # printer! Calling str(  ) on an unknown object could cause an
-            # error we don't want, so we must use try/except to catch it --
-            # we can't stop it from happening, but we can and should
-            # stop it from propagating if it does happen!
-            try:
-                print(value)
-            except:
-                print("<ERROR WHILE PRINTING VALUE>")
+    def __eq__(self, other):
+        return True
 
-def stackdump(id='', msg='HERE'):
-    # frames = inspect.trace()
-    # argvalues = inspect.getargvalues(frames[0][0])
-    # print(sss)
-    # print('ENTERING STACK_DUMP' + (': '+id) if id else '')
-    raw_tb = traceback.extract_stack()
-    entries = traceback.format_list(raw_tb)
+    def __hash__(self):
+        return 0
 
-    frame = sys._getframe(1)
-    sss = inspect.getargvalues(frame)
-
-    # Remove the last two entries for the call to extract_stack() and to
-    # the one before that, this function. Each entry consists of single
-    # string with consisting of two lines, the script file path then the
-    # line of source code making the call to this function.
-    del entries[-2:]
-
-    # Split the stack entries on line boundaries.
-    lines = list(chain.from_iterable(line.splitlines() for line in entries))
-    if msg:  # Append it to last line with name of caller function.
-        lines[-1] += ' <-- ' + str(msg)
-        # lines.append('LEAVING STACK_DUMP' + (': '+id) if id else '')
-    print('\n'.join(lines))
-    print()
-
-sys.modules[__name__] = stackdump  # Make a callable module.
-
-def lru_cache_ignoring_first_argument(*args, **kwargs):
+def lru_cache_ignoring_argument(*args, **kwargs):
     lru_decorator = functools.lru_cache(*args, **kwargs)
 
     def decorator(f):
         @lru_decorator
-        def helper(arg1, *args, **kwargs):
-            return f(arg1, *args, **kwargs)
+        def helper(theself, l, r, box, **kwargs):
+            return f(theself, l, r, box.parent_l, box.parent_r, box.pid, box.id, **kwargs)
 
         @functools.wraps(f)
-        def function(arg1, *args, **kwargs):
-            # print(arg1)
-            print(*args)
-            # print(**kwargs)
-            stackdump(*args)
-            # print_exc_plus()
-            return helper(arg1, *args, **kwargs)
+        def function(theself, l, r, parent_l, parent_r, pid, id, **kwargs):
+            box = _MEquals(parent_l, parent_r, pid, id)
+            return helper(theself, l, r, box, **kwargs)
 
         return function
 
     return decorator
+
+# class _Equals(object):
+#     def __init__(self, o):
+#         self.obj = o
+#
+#     def __eq__(self, other):
+#         return True
+#
+#     def __hash__(self):
+#         return 0
+#
+# def lru_cache_ignoring_first_argument(*args, **kwargs):
+#     lru_decorator = functools.lru_cache(*args, **kwargs)
+#
+#     def decorator(f):
+#         @lru_decorator
+#         def helper(arg1, *args, **kwargs):
+#             arg1 = arg1.obj
+#             return f(arg1, *args, **kwargs)
+#
+#         @functools.wraps(f)
+#         def function(arg1, *args, **kwargs):
+#             arg1 = _Equals(arg1)
+#             return helper(arg1, *args, **kwargs)
+#
+#         return function
+#
+#     return decorator
 
 def my_lru_cache(*args, **kwargs):
     def decorator(f):
@@ -102,17 +74,21 @@ def my_lru_cache(*args, **kwargs):
 
     return decorator
 
+from graphviz import Digraph
+g = Digraph('G', filename='dp.gv', format='svg')
 nodes = []
 def graph_lru_cache(*args, **kwargs):
     def decorator(f):
-        @functools.lru_cache(*args)
+        @lru_cache_ignoring_argument(*args)
+        # @functools.lru_cache(*args)
         def function(*args, **kwargs):
-            _, l, r, parent_l, parent_r = args
-            node_label = f'{l}-{r}'
-            node_label_parent = f'{parent_l}-{parent_r}'
+            _, l, r, parent_l, parent_r, pid, id = args
+            node_label = f'[{l}, {r}]{id}'
+            node_label_parent = f'[{parent_l}, {parent_r}]{pid}'
             if not node_label in nodes:
                 nodes.append(node_label)
             print(f'{node_label} -> {node_label_parent}')
+            g.edge(node_label, node_label_parent)
 
             return f(*args, **kwargs)
 
@@ -134,25 +110,36 @@ class Solution:
 
 
     @graph_lru_cache(maxsize=None)
-    def maxDiff(self, l: int, r:int, parent_l: int, parent_r: int) -> int:
+    def maxDiff(self, l: int, r:int, parent_l: int, parent_r: int, pid: int, id: int) -> int:
+        self.id += 1
         if l == r:
             return self.nums[l]
-        return max(self.nums[l] - self.maxDiff(l + 1, r, l, r), self.nums[r] - self.maxDiff(l, r - 1, l, r))
+        max1 = self.nums[l] - self.maxDiff(l + 1, r, l, r, id, self.id)
+        self.id += 1
+        max2 = self.nums[r] - self.maxDiff(l, r - 1, l, r, id, self.id)
+        return max(max1, max2)
 
     def PredictTheWinner(self, nums: List[int]) -> bool:
         self.nums = nums
-        return self.maxDiff(0, len(nums) - 1, None, None) >= 0
+        self.id = 1
+        return self.maxDiff(0, len(nums) - 1, 0, len(nums)-1, 0, self.id) >= 0
 
 
 def run_graphviz():
     from graphviz import Digraph
-    g = Digraph('G', filename='hello.gv', format='svg')
+    g = Digraph('G', filename='brute.gv', format='svg')
     g.edge('Hello', 'World')
     g.view()
 
-if __name__ == "__main__":
-    # run_graphviz()
-    nums = [1, 5, 2]
+def draw():
+    nums = [1, 5, 2, 2]
     s = Solution()
     print(s.PredictTheWinner(nums))
+    g.view()
+
+if __name__ == "__main__":
+    draw()
+    # nums = [1, 5, 2]
+    # s = Solution()
+    # print(s.PredictTheWinner(nums))
 

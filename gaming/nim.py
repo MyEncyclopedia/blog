d8615c634f56c3de27d2e324e93d4607f20a4ba1
Frank Ding <dingding303@gmail.com>
2020-06-24 11:30:55 +0800

    nim graph

--- gaming/nim.py
+++ gaming/nim.py
@@ -1,3 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_coor(arr):
+    t = list(map(lambda x: 1 if x else None, arr))
+    f = list(map(lambda x: 2 if not x else None, arr))
+    fig, ax = plt.subplots()
+    pt = ax.plot(t, '*', markersize=10)
+    pf = ax.plot(f, '.', markersize=10)
+    ax.legend(['Win', 'Lose'], loc='upper left')
+    plt.yticks([0, 1, 2, 3])
+    plt.xticks(range(len(arr)))
+    # ax.set_ylim(0, 3)
+    plt.show()
+
 
 
 def canWin(n):
@@ -9,5 +24,6 @@ def canWin(n):
     return False
 
 if __name__ == "__main__":
-    for i in range(1, 20):
-        print(canWin(i))
+    aa = [canWin(i) for i in range(0, 20)]
+    # plot_matrix(aa)
+    plot_coor(aa)

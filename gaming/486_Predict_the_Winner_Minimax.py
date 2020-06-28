51b4708ecec5e0e2d550a9ff13dc71a988be0b0b
Frank <dingding303@gmail.com>
2020-06-28 15:02:50 +0800

    486 adjust

--- gaming/486_Predict_the_Winner_Minimax.py
+++ gaming/486_Predict_the_Winner_Minimax.py
@@ -2,15 +2,21 @@ from functools import lru_cache
 from typing import List
 
 class Solution:
+    # max_player: max(A - B)
+    # min_player: min(A - B)
     @lru_cache(maxsize=None)
     def minimax(self, l: int, r: int, isMaxPlayer: bool) -> int:
         if l == r:
             return self.nums[l]
 
         if isMaxPlayer:
-            return max(self.nums[l] + self.minimax(l + 1, r, not isMaxPlayer), self.nums[r] + self.minimax(l, r - 1, not isMaxPlayer))
+            return max(
+                self.nums[l] + self.minimax(l + 1, r, not isMaxPlayer),
+                self.nums[r] + self.minimax(l, r - 1, not isMaxPlayer))
         else:
-            return min(self.minimax(l + 1, r, not isMaxPlayer) - self.nums[l], self.minimax(l, r - 1, not isMaxPlayer) - self.nums[r])
+            return min(
+                -self.nums[l] + self.minimax(l + 1, r, not isMaxPlayer),
+                -self.nums[r] + self.minimax(l, r - 1, not isMaxPlayer))
 
     def PredictTheWinner(self, nums: List[int]) -> bool:
         self.nums = nums

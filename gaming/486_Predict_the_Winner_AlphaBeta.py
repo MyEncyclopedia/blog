162c1f28792fbfb8f2ecd313567e5a438d1207b5
Frank <dingding303@gmail.com>
2020-06-28 21:04:30 +0800

    486 alpha beta finalized

--- gaming/486_Predict_the_Winner_AlphaBeta.py
+++ gaming/486_Predict_the_Winner_AlphaBeta.py
@@ -3,60 +3,28 @@ from functools import lru_cache
 from typing import List
 
 class Solution:
-    # max_player: max(A - B)
-    # min_player: min(A - B)
-    # @lru_cache(maxsize=None)
-    def alpha_beta(self, l: int, r: int, isMaxPlayer: bool, alpha: int, beta: int) -> int:
+    def alpha_beta(self, l: int, r: int, curr: int, isMaxPlayer: bool, alpha: int, beta: int) -> int:
         if l == r:
-            return self.nums[l] * (1 if isMaxPlayer else -1)
+            return curr + self.nums[l] * (1 if isMaxPlayer else -1)
 
         if isMaxPlayer:
-            choiceLeft = self.nums[l] + self.alpha_beta(l + 1, r, not isMaxPlayer, alpha, beta)
-            alpha = max(alpha, choiceLeft)
-            if alpha >= beta:
-                # print(f'{choiceLeft}: {l}-{r} {isMaxPlayer}')
-                return alpha
-            choiceRight = self.nums[r] + self.alpha_beta(l, r - 1, not isMaxPlayer, alpha, beta)
-            # return max(choiceLeft, choiceRight)
-            v = max(choiceLeft, choiceRight)
-            # print(f'{v}: {l}-{r} {isMaxPlayer}')
-            return v
-        else:
-            choiceLeft = -self.nums[l] + self.alpha_beta(l + 1, r, not isMaxPlayer, alpha, beta)
-            beta = min(beta, choiceLeft)
-            if alpha >= beta:
-                # print(f'{choiceLeft}: {l}-{r} {isMaxPlayer}')
-                return beta
-
-            choiceRight = -self.nums[r] + self.alpha_beta(l, r - 1, not isMaxPlayer, alpha, beta)
-            v = min(choiceLeft, choiceRight)
-            # print(f'{v}: {l}-{r} {isMaxPlayer}')
-            return v
-
-    def alpha_beta_2(self, l: int, r: int, isMaxPlayer: bool, alpha: int, beta: int, pre: int) -> int:
-        if l == r:
-            return pre + self.nums[l] * (1 if isMaxPlayer else -1)
-
-        if isMaxPlayer:
-            ret = self.alpha_beta_2(l + 1, r, not isMaxPlayer, alpha, beta, pre + self.nums[l])
+            ret = self.alpha_beta(l + 1, r, curr + self.nums[l], not isMaxPlayer, alpha, beta)
             alpha = max(alpha, ret)
             if alpha >= beta:
                 return alpha
-            ret = max(ret, self.alpha_beta_2(l, r - 1, not isMaxPlayer, alpha, beta, pre + self.nums[r]))
+            ret = max(ret, self.alpha_beta(l, r - 1, curr + self.nums[r], not isMaxPlayer, alpha, beta))
             return ret
         else:
-            ret = self.alpha_beta_2(l + 1, r, not isMaxPlayer, alpha, beta, pre - self.nums[l])
+            ret = self.alpha_beta(l + 1, r, curr - self.nums[l], not isMaxPlayer, alpha, beta)
             beta = min(beta, ret)
             if alpha >= beta:
                 return beta
-            ret = min(ret, self.alpha_beta_2(l, r - 1, not isMaxPlayer, alpha, beta, pre - self.nums[r]))
+            ret = min(ret, self.alpha_beta(l, r - 1, curr - self.nums[r], not isMaxPlayer, alpha, beta))
             return ret
 
     def PredictTheWinner(self, nums: List[int]) -> bool:
         self.nums = nums
-        # v = self.alpha_beta(0, len(nums) - 1, True, -100000000, 100000000)
-        v = self.alpha_beta_2(0, len(nums) - 1, True, -100000000, 100000000, 0)
-        print(f'v={v}')
+        v = self.alpha_beta(0, len(nums) - 1, 0, True, -math.inf, math.inf)
         return v >= 0
 
 

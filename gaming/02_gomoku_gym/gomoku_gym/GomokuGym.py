41ecb9cc94dd9d3a41be72d46413ad037b0b5b28
Frank Ding <dingding303@gmail.com>
2020-07-01 18:04:31 +0800

    GomokuGym

---
+++ gaming/02_gomoku_gym/gomoku_gym/GomokuGym.py
@@ -0,0 +1,82 @@
+import gym
+from gym import spaces
+
+from gomoku_gym.Chessboard import Chessboard
+
+
+class GomokuEnv(gym.Env):
+
+    def __init__(self, grid_num=5, connect_num=3):
+        self.grid_num = grid_num
+        self.connect_num = connect_num
+        self.action_space = spaces.Discrete(grid_num* grid_num)
+        self.observation_space = spaces.Discrete(grid_num * grid_num)
+
+        self.board_game = Chessboard(grid_num=self.grid_num, connect_num=self.connect_num)
+
+        self.seed()
+        self.reset()
+
+    def reset(self):
+        # self.board = [0] * self.grid_num * self.grid_num
+        self.done = False
+
+        self.board_game = Chessboard(grid_num=self.grid_num, connect_num=self.connect_num)
+        return self._get_obs()
+
+    def step(self, action):
+        """Step environment by action.
+
+        Args:
+            action (int): Location
+
+        Returns:
+            list: Obeservation
+            int: Reward
+            bool: Done
+            dict: Additional information
+        """
+        assert self.action_space.contains(action)
+
+        loc = action
+        if self.done:
+            return self._get_obs(), 0, True, None
+
+        reward = NO_REWARD
+        # place
+        self.board[loc] = tocode(self.mark)
+        status = check_game_status(self.board)
+        logging.debug("check_game_status board {} mark '{}'"
+                      " status {}".format(self.board, self.mark, status))
+        if status >= 0:
+            self.done = True
+            if status in [1, 2]:
+                # always called by self
+                reward = O_REWARD if self.mark == 'O' else X_REWARD
+
+        # switch turn
+        self.mark = next_mark(self.mark)
+        return self._get_obs(), reward, self.done, None
+
+    def _get_obs(self):
+        return tuple(self.board), self.mark
+
+    def render(self, mode='human', close=False):
+        self.game.chessboard.action_done = False
+        while not self.game.chessboard.action_done:
+            self.game.update()
+            self.game.draw()
+            self.game.clock.tick(60)
+        print('quit render')
+        return self.id
+        # if close:
+        #     return
+        # if mode == 'human':
+        #     self._show_board(print)  # NOQA
+        #     print('')
+        # else:
+        #     self._show_board(logging.info)
+        #     logging.info('')
+
+    def available_actions(self):
+        return [i for i, c in enumerate(self.board) if c == 0]
--- gaming/02_gomoku_gym/gomoku_gym/PyGameChessboard.py
+++ gaming/02_gomoku_gym/gomoku_gym/PyGameChessboard.py
@@ -17,6 +17,39 @@ class Chessboard:
         self.grid = []
         for i in range(self.grid_count):
             self.grid.append(list("." * self.grid_count))
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((800, 600))
+        pygame.display.set_caption("TTT")
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.Font(pygame.font.get_default_font(), 24)
+        self.going = True
+
+
+    def loop(self):
+        while self.going:
+            self.update()
+            self.draw_()
+            self.clock.tick(60)
+
+        pygame.quit()
+
+    def update(self):
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                self.going = False
+            elif e.type == pygame.MOUSEBUTTONDOWN:
+                self.handle_key_event(e)
+
+    def draw_(self):
+        self.screen.fill((255, 255, 255))
+        self.screen.blit(self.font.render("FPS: {0:.2F}".format(self.clock.get_fps()), True, (0, 0, 0)), (10, 10))
+
+        self.draw(self.screen)
+        if self.game_over:
+            self.screen.blit(self.font.render("{0} Win".format("Black" if self.winner == 'b' else "White"), True, (0, 0, 0)), (500, 10))
+
+        pygame.display.update()
 
     def handle_key_event(self, e):
         origin_x = self.start_x - self.edge_size
@@ -105,3 +138,8 @@ class Chessboard:
                     x = self.start_x + c * self.grid_size
                     y = self.start_y + r * self.grid_size
                     pygame.draw.circle(screen, color, [x, y], self.grid_size // 2)
+
+
+if __name__ == '__main__':
+    game = Chessboard()
+    game.loop()

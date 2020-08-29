b3cc11bada1dc8615cd5c4a946e2102d433b3260
Frank <dingding303@gmail.com>
2020-08-29 20:08:04 +0800

    1227 code

--- leetcode/1227_sim.py
+++ leetcode/1227_sim.py
@@ -18,7 +18,7 @@ def simulate_bruteforce(n: int) -> bool:
                 seats[i] = True
             else:
                 while True:
-                    rnd = random.randint(0, n - 1)
+                    rnd = random.randint(0, n - 1) # random until no conflicts
                     if not seats[rnd]:
                         seats[rnd] = True
                         break
@@ -39,7 +39,7 @@ def simulate_online(n: int) -> bool:
         seats[i] = seats[j]
         seats[j] = tmp
 
-    # for each person, the seats are [i, n-1]
+    # for each person, the seats array idx available are [i, n-1]
     for i in range(n-1):
         if i == 0:  # first one, always random
             rnd = random.randint(0, n - 1)
@@ -53,6 +53,32 @@ def simulate_online(n: int) -> bool:
     return seats[n-1] == n - 1
 
 
+def simulate(n: int) -> bool:
+    """
+    Simulates one round of complexity O(N).
+    :param n: total number of seats
+    :return: True if last one has last seat, otherwise False
+    """
+
+    seats = [False for _ in range(n)]
+
+    # for each person, the seats array idx available are [i, n-1]
+    for i in range(n-1):
+        if i == 0:  # first one, always random
+            rnd = random.randint(0, n - 1)
+            seats[rnd] = True
+        else:
+            if not seats[i]:  # i-th still has his seat
+                seats[i] = True
+            else:
+                # 0 must not be available, now we have 0 and [i+1, n-1],
+                rnd = random.randint(i, n - 1)
+                if rnd == i:
+                    seats[0] = True
+                else:
+                    seats[rnd] = True
+    return not seats[n-1]
+
 def plot(y):
     import matplotlib.pyplot as plt
     import seaborn as sns
@@ -61,7 +87,7 @@ def plot(y):
     x = np.arange(len(y))
 
     ax = sns.pointplot(x=x[1:], y=y[1:], join=False)
-    ax.set_title("1227")
+    ax.set_title("1227 Probability")
 
     plt.show()
 
@@ -70,9 +96,9 @@ if __name__ == "__main__":
     sample_size = 500
     for n in range(1, 21):
         # total = sum(1 if simulate_online(n) else 0 for _ in range(500))
-        total = sum(1 if simulate_bruteforce(n) else 0 for _ in range(sample_size))
+        total = sum(1 if simulate(n) else 0 for _ in range(sample_size))
         stat[n] = total / sample_size
+        print(f'{n} => {stat[n]}')
 
-    print(f'{stat}')
     plot(stat)
 
